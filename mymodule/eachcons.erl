64e0edd7d6a1caed2ad7dcaf2ecca53a71b895fe
Elias Dorneles <eliasdorneles@gmail.com>
2014-02-19 07:57:14 -0400

    adicionado makefile e testes

--- mymodule/eachcons.erl
+++ mymodule/eachcons.erl
@@ -1,11 +1,17 @@
 -module(eachcons).
--export([eachcons/2]).
+-export([eachcons/2, test/0]).
 
 eachcons(L, N) ->
-    eachcons(N, L, []).
+    eachcons(L, N, []).
 
 eachcons(L, N, Result) when length(L) < N ->
     lists:reverse(Result);
 
 eachcons(L = [_|T], N, Result) ->
     eachcons(T, N, [lists:sublist(L, N)|Result]).
+
+test() ->
+    [[1, 2], [2, 3], [3, 4]] = eachcons([1, 2, 3, 4], 2),
+    [[1, 2, 3], [2, 3, 4]] = eachcons([1, 2, 3, 4], 3),
+    [[1, 2, 3], [2, 3, 4]] = eachcons([1, 2, 3, 4], 3),
+    all_passed.
