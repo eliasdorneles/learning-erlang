061d3763eec0cdca7df58c5587ca361fdf810a52
Elias Dorneles <eliasdorneles@gmail.com>
2014-03-08 14:45:32 -0300

    fix my_spawn to return Pid

--- chapter13-concurrent_error_handling/zombie.erl
+++ chapter13-concurrent_error_handling/zombie.erl
@@ -11,25 +11,19 @@ on_exit(Pid, Fun) ->
           end).
 
 spawn_with_timeout(Mod, Func, Args, Time) ->
-    Pid = spawn(Mod, Func, Args),
-    timer:sleep(Time),
-    exit(Pid, killed).
-
-my_spawn1(Mod, Func, Args) ->
+    Pid = my_spawn(Mod, Func, Args),
     spawn(fun() ->
-                  Start = now(),
-                  {Pid, Ref} = spawn_monitor(Mod, Func, Args),
-                  receive
-                      {'DOWN', Ref, process, Pid, Why} ->
-                          End = now(),
-                          io:format("Process ~p died after ~p us, with cause: ~p~n", [Ref, timer:now_diff(End, Start), Why])
-                  end
-          end).
+                  timer:sleep(Time),
+                  exit(Pid, killed)
+          end),
+    Pid.
 
-my_spawn2(Mod, Func, Args) ->
+my_spawn(Mod, Func, Args) ->
     Start = erlang:now(),
-    on_exit(spawn(Mod, Func, Args),
+    Pid = spawn(Mod, Func, Args),
+    on_exit(Pid,
             fun(Why) ->
                     End = erlang:now(),
                     io:format("Process died after ~p us, with cause: ~p~n", [timer:now_diff(End, Start), Why])
-            end).
+            end),
+    Pid.
